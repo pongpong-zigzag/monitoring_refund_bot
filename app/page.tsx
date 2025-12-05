37c10686e593b3869e4d71b7fc7172e6bfd1af95
pongpong-zigzag <177026+pongpong-zigzag@users.noreply.github.com>
2025-12-05 16:14:33 +0900

    feat: change transfer qubic token function

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,29 @@
 "use client";
 
 import React, { useState } from "react";
-import { AccBalance } from "./type/interface";
-import { getAccBalance } from "@/lib/getAccInfo";
+import { AccBalance, TickInfo } from "./type/interface";
+import { getAccBalance, getTickInfo } from "@/lib/getAccInfo";
 
 const RPC_URL = "https://rpc.qubic.org";
 const MY_ACCOUNT_ID = "FTHFVJVMZWFOQEYAZPYSXASIRMXCPEUVCFQKZVTKXEXXLYKSYJRZQQEHGDPN";
 const MY_ACCOUNT_SEED = "hbxvaraghtrqxvxizohsvxbmqsiouwurtmskdvzmmcqemlkoxspspql";
-const DEFAULT_POLL_INTERVAL = 5_000;
-const QUBIC_PER_QXMR = BigInt(100);
 
 export default function Home() {
 
   const [balance, setBalance] = useState<AccBalance | null>(null);
+  const [tickInfo, setTickInfo] = useState<TickInfo[] | null>(null);
 
   React.useEffect(() => {
     const load = async () => {
-      const balance = await getAccBalance(RPC_URL, MY_ACCOUNT_ID);
-      setBalance(balance);
-      console.log(balance);
+      const accInfo = await getAccBalance(RPC_URL, MY_ACCOUNT_ID);
+      setBalance(accInfo);
+
+      const lastTickInfo = await getTickInfo(RPC_URL, accInfo.latestIncomingTransferTick);
+      setTickInfo(lastTickInfo);
+
+
+      console.log("accInfo:", accInfo);
+      console.log("lastTickInfo:", lastTickInfo);
     };
 
     load();
